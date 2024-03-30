56ef7aebb5ad713b7af761b01dff4389179a7430
Edmharx Macasinag <edmharxx@gmail.com>
2024-03-30 19:02:52 +0800

    Setting the directory and reading the file types

--- main.py
+++ main.py
@@ -3,6 +3,18 @@ import os
 
 
 #set the dir of the files
-#read file type
+directory = (r"C:\Users\edmha\OneDrive\Desktop\Importants")
+
+#checking if the directory exists
+if os.path.exists(directory):
+    #open directory
+    with os.scandir(directory) as entries:
+        for entry in entries:
+            if entry.is_file(): #read file type
+                filename, extension = os.path.splitext(entry.name)
+                print(f"File: {entry.name}, Extension: {extension}")
+else:
+    print("Please type an existing directory")
+
 #create folders depending on file types existing in the dir
 #put the files on the respective folders
