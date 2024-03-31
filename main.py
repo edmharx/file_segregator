7ff534602d65594a19608f30e499c4c51011a8bc
Edmharx Macasinag <edmharxx@gmail.com>
2024-03-31 14:42:23 +0800

    fixed for file to move in respective folders

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import platform
+import shutil
 import os
 
 
@@ -7,23 +7,22 @@ directory = (r"C:\Users\edmha\Downloads\test folder")
 
 #create folders depending on file types existing in the dir
 def create_folder(directory):
-    with os.scandir(directory) as entries: #get the files
-        for entry in entries:
-            if entry.is_file(): #separate filename and extension
-                filename, extension = os.path.splitext(entry.name)
+    with os.scandir(directory) as files: #get the files
+        for file in files:
+            if file.is_file(): #separate filename and extension
+                filename, extension = os.path.splitext(file.name)
                 extension = extension[1:]
 
                 new_folder_name = os.path.join(directory, extension)
                 if not os.path.exists(new_folder_name): #create a folder per extension if it does not exist
                     os.mkdir(new_folder_name)
                 else:
-                    print(f"Folder {new_folder_name} already exists")
-                    
-                file_path = os.path.join(directory, filename)
-
+                    print(f"Folder {extension} already exists")
+                
                 #put the files on the respective folders
-                new_file_path = os.path.join (new_folder_name, filename)
-                os.rename (file_path, new_file_path)
+                file_path = os.path.join(directory, file)
+                shutil.move(file_path, new_folder_name)
+
 
 if os.path.exists(directory):
     create_folder(directory)
